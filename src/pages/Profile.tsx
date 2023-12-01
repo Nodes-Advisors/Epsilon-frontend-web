d5c2742ed3b7e594f6c8695a9005d0a6beed8482
SyLi_4aaf <Shaoyan.li@nodesadvisors.com>
2023-12-01 18:06:09 +0100

    feat: set up basic pages

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,20 @@
+
+
+export default function Profile() {
+    // const { user } = useAuth()
+    
+    // return (
+    //     <div>
+    //     <h1>Profile</h1>
+    //     <div>
+    //         <div>Username: {user?.username}</div>
+    //         <div>Email: {user?.email}</div>
+    //     </div>
+    //     </div>
+    // )
+
+
+    return (
+        <div>user profile</div>
+    )
+}
---
+++ src/router/index.tsx
@@ -0,0 +1,18 @@
+import React, { lazy } from "react"
+import { createBrowserRouter, Navigate } from "react-router-dom"
+
+const Home = lazy(() => import("../pages/Home"))
+const About = lazy(() => import("../pages/About"))
+const NotFound = lazy(() => import("../pages/NotFound"))
+const Profile = lazy(() => import("../pages/Profile"))
+
+const router =  createBrowserRouter([
+    { index: true, element: <Navigate to='/home' /> },
+    { path: "/", element: <Navigate to='/home' /> },
+    { path: "home", element: <Home /> },
+    { path: "about", element: <About /> },
+    { path: "profile", element: <Profile /> },
+    { path: "*", element: <NotFound /> },
+])
+
+export default router
