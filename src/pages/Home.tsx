ef1853820c5589592dd409e71155dd019884533b
SyLi_4aaf <79486258+SyLi9527@users.noreply.github.com>
2023-12-05 17:28:48 +0100

    feat: add home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,30 @@
+import styles from '../styles/home.module.less'
+import HeadImg from '../assets/images/headimg-example.png'
+import StatusIcon from '../assets/svgs/status-icon.svg?react'
+import EpsilonMiniLogo from '../assets/svgs/epsilon-mini-logo.svg?react'
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>   
-    </div>
+    <section className={styles['relative-layout']}>
+      <aside className={styles['left-panel']}>
+        <div className={styles['epsilon-blur-circle']}>
+          <EpsilonMiniLogo className={styles['epsilon-mini-logo']}/>
+        </div>
+        <div className={styles['headimg-wrapper']}>
+          <img src={HeadImg} alt="" className={styles['headimg']}  />
+          <StatusIcon className={`${styles['status-icon']} ${styles['status-leave']}`} />
+        </div>
+        <div className={styles['headimg-wrapper']}>
+          <img src={HeadImg} alt="" className={styles['headimg']}  />
+          <StatusIcon className={`${styles['status-icon']} ${styles['status-busy']}`} />
+        </div>
+        <div className={styles['headimg-wrapper']}>
+          <img src={HeadImg} alt="" className={styles['headimg']}  />
+          <StatusIcon className={`${styles['status-icon']} ${styles['status-free']}`} />
+        </div>
+        <div>
+
+        </div>
+      </aside>
+    </section>
   )
 }
