b9470e1beb19c24610649651418800fa4beee5fc
SyLi_4aaf <shaoyan.li@uzh.ch>
2024-02-12 19:04:52 +0100

    feat: remove 404 for intelligence page

---
+++ src/pages/Intelligence.tsx
@@ -0,0 +1,10 @@
+import IntelligenceImg from '../assets/images/broken_robot.png'
+
+export default function Intelligence() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      {/* <h1>Intelligence</h1> */}
+      <img src={IntelligenceImg} alt="Intelligence" />
+    </div>
+  )
+}
