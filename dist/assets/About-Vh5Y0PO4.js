83cc10e8d8874e9b5432e2fbdf753387e0a96b8b
SyLi2795 <152450268+SyLi2795@users.noreply.github.com>
2024-02-23 19:37:10 +0800

    feat: add production code

---
+++ dist/assets/About-Vh5Y0PO4.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-lGblRe-B.js";function r(){return t.jsx("section",{})}export{r as default};
---
+++ dist/assets/DetailMessage-0lJLU8vi.js
@@ -0,0 +1,1 @@
+import{f as i,u as c,j as s}from"./index-lGblRe-B.js";function u(){const{state:e}=i(),t=e,n=c();return s.jsxs("div",{children:[Object.keys(t).map((a,o)=>s.jsxs("h2",{children:[a,": ",t[a]??"null"]},o)),s.jsx("button",{onClick:()=>n(-1),children:"go back"})]})}export{u as default};
